09036a10e6a61cef25bca64e3410abac16d197d8
yuxingfei <yuxingfei@moerlong.com>
2020-11-06 14:33:29 +0800

    ueditor 相关上传文件 文件列表等功能 end

--- controllers/editor.go
+++ controllers/editor.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"beego-admin/services"
+	"net/url"
+	"strings"
 )
 
 type EditorController struct {
@@ -14,22 +16,37 @@ func (this *EditorController) Prepare() {
 }
 
 func (this *EditorController) Server() {
-
 	result := make(map[string]interface{})
+
 	var ueditorService services.UeditorService
 	action := this.GetString("action")
 	switch action {
 	case "config":
 		result = ueditorService.GetConfig()
-	case "uploadimage": //上传图片
+	case "uploadimage":
+		//上传图片
 		result = ueditorService.UploadImage(this.Ctx)
-	case "uploadscrawl": //上传涂鸦
-	case "uploadvideo": //上传视频
-	case "uploadfile": //上传文件
-	case "listimage": //列出图片
+	case "uploadscrawl":
+		//上传涂鸦
+		//带+号的值如果不处理，会被转为空格
+		this.Ctx.Request.URL.RawQuery = strings.ReplaceAll(this.Ctx.Request.URL.RawQuery, "+", "%2b")
+		values, _ := url.ParseQuery(this.Ctx.Request.URL.RawQuery)
+		result = ueditorService.UploadScrawl(values)
+	case "uploadvideo":
+		//上传视频
+		result = ueditorService.UploadVideo(this.Ctx)
+	case "uploadfile":
+		//上传文件
+		result = ueditorService.UploadFile(this.Ctx)
+	case "listimage":
+		//列出图片
 		result = ueditorService.ListImage(this.Input())
-	case "listfile": //列出文件
-	case "catchimage": //抓取远程文件
+	case "listfile":
+		//列出文件
+		result = ueditorService.ListFile(this.Input())
+	case "catchimage":
+		//抓取远程文件
+		result = ueditorService.CatchImage(this.Ctx)
 	default:
 		this.Data["json"] = map[string]string{
 			"state": "请求地址出错",
--- services/ueditor_service.go
+++ services/ueditor_service.go
@@ -1,13 +1,14 @@
 package services
 
 import (
-	"fmt"
+	"encoding/base64"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
 	"github.com/google/uuid"
 	"gopkg.in/ini.v1"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"net/url"
 	"os"
 	"path"
@@ -53,8 +54,7 @@ func (this *UeditorService) UploadImage(ctx *context.Context) map[string]interfa
 			"state": "not found field ueditor::imageFieldName.",
 		}
 	}
-	result := this.upFile(fieldName, ctx)
-	return result
+	return this.upFile(fieldName, ctx)
 }
 
 //上传文件
@@ -74,7 +74,7 @@ func (*UeditorService) upFile(fieldName string, ctx *context.Context) map[string
 		return result
 	}
 
-	//数据表写入
+	//存储名
 	saveName := uuid.New().String()
 	//后缀带. (.png)
 	fileExt := path.Ext(h.Filename)
@@ -122,8 +122,8 @@ func (this *UeditorService) ListImage(get url.Values) map[string]interface{} {
 		result["state"] = "config params error."
 	}
 
-	listSizeInt,_ := strconv.Atoi(listSize)
-	result = this.fileList(allowFiles,listSizeInt,get)
+	listSizeInt, _ := strconv.Atoi(listSize)
+	result = this.fileList(allowFiles, listSizeInt, get)
 
 	return result
 }
@@ -174,7 +174,7 @@ func (this *UeditorService) fileList(allowFiles string, listSize int, get url.Va
 		"total": lenFiles,
 	}
 
-	if startInt > lenFiles || end < 0 || startInt > end{
+	if startInt > lenFiles || end < 0 || startInt > end {
 		result["list"] = map[string]interface{}{}
 	}
 
@@ -182,7 +182,7 @@ func (this *UeditorService) fileList(allowFiles string, listSize int, get url.Va
 
 	if end > lenFiles {
 		endInt = lenFiles
-	}else {
+	} else {
 		endInt = end
 	}
 
@@ -191,6 +191,7 @@ func (this *UeditorService) fileList(allowFiles string, listSize int, get url.Va
 	return result
 }
 
+//遍历获取目录下的指定类型的文件
 func (this *UeditorService) getFiles(dir, allowFiles string) []map[string]string {
 	path := filepath.ToSlash(beego.AppPath + dir)
 	var filesArr []map[string]string
@@ -216,6 +217,9 @@ func (this *UeditorService) getFiles(dir, allowFiles string) []map[string]string
 				filesArr = append(filesArr, childFilesArr...)
 			}
 		} else {
+			if !strings.Contains(allowFiles, strings.ToLower(strings.TrimLeft(filepath.Ext(file.Name()), "."))) {
+				continue
+			}
 			filesArr = append(filesArr, map[string]string{
 				"url":   dir + file.Name(),
 				"mtime": strconv.Itoa(int(file.ModTime().Unix())),
@@ -224,5 +228,293 @@ func (this *UeditorService) getFiles(dir, allowFiles string) []map[string]string
 	}
 
 	return filesArr
+}
+
+//上传视频
+func (this *UeditorService) UploadVideo(ctx *context.Context) map[string]interface{} {
+	fieldName := beego.AppConfig.String("ueditor::videoFieldName")
+	if fieldName == "" {
+		return map[string]interface{}{
+			"state": "not found field ueditor::videoFieldName.",
+		}
+	}
+
+	return this.upFile(fieldName, ctx)
+}
+
+//上传文件
+func (this *UeditorService) UploadFile(ctx *context.Context) map[string]interface{} {
+	fieldName := beego.AppConfig.String("ueditor::fileFieldName")
+	if fieldName == "" {
+		return map[string]interface{}{
+			"state": "not found field ueditor::fileFieldName.",
+		}
+	}
+
+	return this.upFile(fieldName, ctx)
+}
+
+//列出文件
+func (this *UeditorService) ListFile(get url.Values) map[string]interface{} {
+	result := make(map[string]interface{})
+	allowFiles := beego.AppConfig.String("ueditor::fileManagerAllowFiles")
+	listSize := beego.AppConfig.String("ueditor::fileManagerListSize")
+	if allowFiles == "" || listSize == "" || len(get) <= 0 {
+		result["state"] = "config params error."
+	}
+
+	listSizeInt, _ := strconv.Atoi(listSize)
+	result = this.fileList(allowFiles, listSizeInt, get)
+
+	return result
+}
+
+//上传涂鸦
+func (this *UeditorService) UploadScrawl(get url.Values) map[string]interface{} {
+	result := make(map[string]interface{})
+	pathFormat := beego.AppConfig.String("ueditor::scrawlPathFormat")
+	maxSize := beego.AppConfig.String("ueditor::scrawlMaxSize")
+	allowFiles := beego.AppConfig.String("ueditor::scrawlAllowFiles")
+	oriName := beego.AppConfig.String("ueditor::scrawlFieldName")
+
+	if pathFormat == "" || maxSize == "" || allowFiles == "" || oriName == "" {
+		result["state"] = "config params error."
+		return result
+	}
+
+	config := map[string]string{
+		"pathFormat": pathFormat,
+		"maxSize":    maxSize,
+		"allowFiles": allowFiles,
+		"oriName":    oriName,
+	}
+
+	base64Data := get.Get(oriName)
+	return this.upBase64(config, base64Data)
+}
+
+//处理base64编码的图片上传
+func (this *UeditorService) upBase64(config map[string]string, base64Data string) map[string]interface{} {
+	result := make(map[string]interface{})
+	imgByte, err := base64.StdEncoding.DecodeString(base64Data)
+	if err != nil {
+		result["state"] = "picture content get error. err:" + err.Error()
+		return result
+	}
+
+	path := "/static/uploads/ueditor/scrawl/"
+	dirName := filepath.ToSlash(beego.AppPath + path)
+	file := make(map[string]string)
+	file["filesize"] = strconv.Itoa(len(string(imgByte)))
+	file["oriName"] = config["oriName"]
+	file["ext"] = ".png"
+	file["name"] = uuid.New().String() + file["ext"]
+	file["fullName"] = dirName + file["name"]
+	file["urlName"] = path + file["name"]
+
+	fullName := file["fullName"]
+
+	//检查文件大小是否超出限制
+	fileSizeInt, _ := strconv.Atoi(file["filesize"])
+	maxSizeInt, _ := strconv.Atoi(config["maxSize"])
+	if fileSizeInt >= maxSizeInt {
+		result["state"] = "文件大小超出网站限制"
+		return result
+	}
+
+	//创建目录失败
+	_, err = os.Stat(dirName)
+	if err != nil {
+		err = os.MkdirAll(dirName, os.ModePerm)
+		if err != nil {
+			result["state"] = "目录创建失败"
+			return result
+		}
+	}
+
+	//写入文件
+	f, err := os.OpenFile(fullName, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		result["state"] = "写入文件内容错误,err :" + err.Error()
+		return result
+	}
+	defer f.Close()
+	_, err = f.Write(imgByte)
+	if err != nil {
+		result["state"] = "写入文件失败,err :" + err.Error()
+		return result
+	}
+
+	result = map[string]interface{}{
+		"state":    "SUCCESS",
+		"url":      file["urlName"],
+		"title":    file["name"],
+		"original": file["oriName"],
+		"type":     file["ext"],
+		"size":     file["filesize"],
+	}
+
+	return result
+}
+
+//抓取远程文件
+func (this *UeditorService) CatchImage(ctx *context.Context) map[string]interface{} {
+	result := make(map[string]interface{})
+	pathFormat := beego.AppConfig.String("ueditor::catcherPathFormat")
+	maxSize := beego.AppConfig.String("ueditor::catcherMaxSize")
+	allowFiles := beego.AppConfig.String("ueditor::catcherAllowFiles")
+	oriName := "remote.png"
+
+	if pathFormat == "" || maxSize == "" || allowFiles == "" {
+		result["state"] = "config params error."
+		return result
+	}
+
+	config := map[string]string{
+		"pathFormat": pathFormat,
+		"maxSize":    maxSize,
+		"allowFiles": allowFiles,
+		"oriName":    oriName,
+	}
+
+	fieldName := beego.AppConfig.String("ueditor::catcherFieldName")
+
+	source := make([]string, 0)
+	ctx.Input.Bind(&source, fieldName)
+
+	var list []map[string]string
+	//没有数据
+	if len(source) <= 0 {
+		result = map[string]interface{}{
+			"state": "ERROR",
+			"list":  list,
+		}
+		return result
+	}
+
+	for _, imgUrl := range source {
+		info := this.saveRemote(config, imgUrl)
+		if info["state"] == "SUCCESS" {
+			list = append(list, map[string]string{
+				"state":    info["state"],
+				"url":      info["url"],
+				"size":     info["size"],
+				"title":    info["title"],
+				"original": info["original"],
+				"source":   imgUrl,
+			})
+		} else {
+			list = append(list, map[string]string{
+				"state":    info["state"],
+				"url":      "",
+				"size":     "",
+				"title":    "",
+				"original": "",
+				"source":   imgUrl,
+			})
+		}
+
+	}
+
+	result = map[string]interface{}{
+		"state": "SUCCESS",
+		"list":  list,
+	}
+
+	return result
+}
+
+//抓取远程图片
+func (*UeditorService) saveRemote(config map[string]string, fieldName string) map[string]string {
+	result := make(map[string]string)
+	imgUrl := strings.ReplaceAll(fieldName, "&amp;", "&")
+
+	if imgUrl == "" {
+		result["state"] = "链接为空"
+		return result
+	}
+
+	//http开头验证
+	if !strings.HasPrefix(imgUrl, "http") {
+		result["state"] = "链接不是http链接"
+		return result
+	}
+
+	//获取请求头并检测死链
+	response, err := http.Get(imgUrl)
+	defer response.Body.Close()
+	if err != nil || response.StatusCode != 200 {
+		result["state"] = "链接不可用"
+		return result
+	}
+
+	//格式验证(扩展名验证和Content-Type验证)
+	if !strings.Contains(response.Header.Get("Content-Type"), "image") {
+		result["state"] = "链接contentType不正确"
+		return result
+	}
+
+	fileType := strings.TrimLeft(filepath.Ext(imgUrl), ".")
+	if fileType == "" || !strings.Contains(config["allowFiles"], fileType) {
+		result["state"] = "链接url后缀不正确"
+		return result
+	}
+
+	path := "/static/uploads/ueditor/remote/"
+	dirName := filepath.ToSlash(beego.AppPath + path)
+
+	file := make(map[string]string)
+	file["oriName"] = filepath.Ext(imgUrl)
+	file["filesize"] = "0"
+	file["ext"] = file["oriName"]
+	file["name"] = uuid.New().String() + file["ext"]
+	file["fullName"] = dirName + file["name"]
+	file["urlName"] = path + file["name"]
+
+	//检查文件大小是否超出限制
+	fileSizeInt, _ := strconv.Atoi(file["filesize"])
+	maxSizeInt, _ := strconv.Atoi(config["maxSize"])
+	if fileSizeInt >= maxSizeInt {
+		result["state"] = "文件大小超出网站限制"
+		return result
+	}
+
+	//创建目录失败
+	_, err = os.Stat(dirName)
+	if err != nil {
+		err = os.MkdirAll(dirName, os.ModePerm)
+		if err != nil {
+			result["state"] = "目录创建失败"
+			return result
+		}
+	}
+
+	//写入文件
+	img := response.Body
+	f, err := os.Create(file["fullName"])
+
+	if err != nil {
+		result["state"] = "写入文件失败,err :" + err.Error()
+		return result
+	}
+
+	w, err := io.Copy(f, img)
+	if err != nil {
+		result["state"] = "写入文件失败,err :" + err.Error()
+		return result
+	}
+
+	file["filesize"] = strconv.Itoa(int(w))
+
+	result = map[string]string{
+		"state":    "SUCCESS",
+		"url":      file["urlName"],
+		"title":    file["name"],
+		"original": file["oriName"],
+		"type":     file["ext"],
+		"size":     file["filesize"],
+	}
+
+	return result
 
 }
